fffee952115a52052d7c6cd835039c9d58d16a02
nhartland <nhartland@a61efc3c-2a36-418a-8ad6-9b3ff2163bc2>
2014-07-31 18:24:26 +0000

    tidy
    git-svn-id: https://svn.hepforge.org/mcgridsvn/trunk@15 a61efc3c-2a36-418a-8ad6-9b3ff2163bc2

--- mcgrid/scripts/identifySubprocs.py
+++ mcgrid/scripts/identifySubprocs.py
@@ -3,7 +3,7 @@
 """
     identifySubprocs.py
     
-    Reads COMIX .map files for all 2->N process subdirectories
+    Reads COMIX/AMEGIC info files for all 2->N processes
     and builds a MadGraph PDF combination file for use with
     appl::lumi_pdf
   
@@ -15,7 +15,34 @@ import sys
 import getopt
 import shutil
 
-import unpack
+# *************** Database Unpack ******************
+
+import sqlite3
+import sys
+
+def unpack(procDir,procDB):
+  # Fetch process DB
+  sql_conn = sqlite3.connect(procDB)
+  sql_cur = sql_conn.cursor()
+  
+  # query
+  query="SELECT * FROM path;"
+  sql_cur.execute(query)
+  
+  # Write out packed files
+  for idx, row in enumerate(sql_cur):
+    
+    name = row[0].split('/')[-1]
+    cont = row[1]
+    
+    flname = "{0}/{1}".format(procDir,name)
+    output = open(flname, "wt")
+    output.write(str(cont))
+    
+    output.close()
+
+# **************************************************
+
 
 def usage():
   print 'Usage: '+sys.argv[0]+' <Process Database> [-b pp/ppbar/pbarp]'
